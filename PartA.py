b073b8af955284500bbea141e27b9a65fada6ee8
Antony Chen <achen321@calpoly.edu>
2026-01-13 21:28:00 -0800

    Update PartA.py

--- PartA.py
+++ PartA.py
@@ -40,3 +40,46 @@ size2 = os.path.getsize('images/group_photo.jpg')
 
 print(f"File Size - Landscape: {size1} bytes")
 print(f"File Size - Group Photo: {size2} bytes")
+
+
+#----------------------Part 5---------------------------------------------
+
+def reduce_resolution(image):
+    reduce_image = []
+    temp_y = []
+    for x in range(image.shape[0]):
+        if x % 2 != 0:
+            continue
+        for y in range(image.shape[1]):
+            if y % 2 == 0:
+                temp_y.append(image[x][y])
+        reduce_image.append(temp_y)
+        temp_y = []
+    reduce_image = np.array(reduce_image)
+    return reduce_image
+
+
+image = cv2.imread("images/landscape.png")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Displayed Image",gray_image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+reduce1 = reduce_resolution(gray_image)
+cv2.imshow("Reduce1",reduce1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+reduce2 = reduce_resolution(reduce1)
+cv2.imshow("Reduce2",reduce2)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+reduce3 = reduce_resolution(reduce2)
+cv2.imshow("Reduce2",reduce3)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
